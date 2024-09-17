bf66229f2fa67556d3a43da253ae0c519267cca2
Slava Maksimov <hello@slavamak.com>
2024-09-17 21:18:21 +0300

    chore: prettify code

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
-import { Telegraf } from 'telegraf';
-import { fmt, bold, link } from 'telegraf/format';
+import { Telegraf } from 'telegraf'
+import { fmt, bold, link } from 'telegraf/format'
 
 const commands = [
   { command: 'start', description: 'Start interacting with the bot' },
@@ -7,13 +7,13 @@ const commands = [
   { command: 'remove_filter', description: 'Remove a filter by its index' },
   { command: 'list_filters', description: 'List all active filters' },
   { command: 'help', description: 'Get help about using the bot' },
-];
+]
 
-const filters = {};
-const lastTasks = {};
+const filters = {}
+const lastTasks = {}
 
-const bot = new Telegraf(process.env.BOT_TOKEN);
-bot.command('start', (ctx) => ctx.reply('Welcome'));
+const bot = new Telegraf(process.env.BOT_TOKEN)
+bot.command('start', (ctx) => ctx.reply('Welcome'))
 
 bot.command('help', (ctx) => {
   ctx.reply(fmt`
@@ -26,66 +26,66 @@ ${bold`Welcome to the Habr Freelance task notifier bot!`} Here are the commands 
 /help - Get help about using the bot.
 
 You will receive notifications for new tasks that match your filters!`
-  );
-});
+  )
+})
 
 bot.command('add_filter', (ctx) => {
-  const args = ctx.message.text.split(' ').slice(1);
-  const category = args[0] || '';
-  const query = args[1] || '';
-  const userId = ctx.from.id;
-  if (!filters[userId]) filters[userId] = [];
-  filters[userId].push({ category, query });
+  const args = ctx.message.text.split(' ').slice(1)
+  const category = args[0] || ''
+  const query = args[1] || ''
+  const userId = ctx.from.id
+  if (!filters[userId]) filters[userId] = []
+  filters[userId].push({ category, query })
   const message = `Filter added: category="${category}", query="${query}"`
-  ctx.reply(message);
-  console.log('add_filter', message);
-});
+  ctx.reply(message)
+  console.log('add_filter', message)
+})
 
 bot.command('remove_filter', (ctx) => {
-  const args = ctx.message.text.split(' ').slice(1);
-  const filterIndex = parseInt(args[0]);
-  const userId = ctx.from.id;
+  const args = ctx.message.text.split(' ').slice(1)
+  const filterIndex = parseInt(args[0])
+  const userId = ctx.from.id
   if (filters[userId] && filters[userId][filterIndex]) {
-    filters[userId].splice(filterIndex, 1);
+    filters[userId].splice(filterIndex, 1)
     const message = `Filter ${filterIndex} deleted.`
-    ctx.reply(message);
-    console.log('remove_filter', message);
+    ctx.reply(message)
+    console.log('remove_filter', message)
   } else {
     const message = 'Filter not found.'
-    ctx.reply(message);
-    console.log('remove_filter', message);
+    ctx.reply(message)
+    console.log('remove_filter', message)
   }
-});
+})
 
 bot.command('list_filters', (ctx) => {
-  const userId = ctx.from.id;
+  const userId = ctx.from.id
   if (filters[userId] && filters[userId].length > 0) {
-    const filterList = filters[userId].map((f, index) => `${index}: category="${f.category}", query="${f.query}"`).join('\n');
-    ctx.reply(`Your filters:\n${filterList}`);
+    const filterList = filters[userId].map((f, index) => `${index}: category="${f.category}", query="${f.query}"`).join('\n')
+    ctx.reply(`Your filters:\n${filterList}`)
   } else {
-    ctx.reply('You do not have filters.');
+    ctx.reply('You do not have filters.')
   }
-});
+})
 
 async function checkForNewTasks() {
-  const userIds = Object.keys(filters);
-  const tasksToNotify = [];
+  const userIds = Object.keys(filters)
+  const tasksToNotify = []
   for (const userId of userIds) {
-    const userFilters = filters[userId];
+    const userFilters = filters[userId]
     for (const filter of userFilters) {
-      const category = filter.category || null;
-      const query = filter.query || null;
+      const category = filter.category || null
+      const query = filter.query || null
       const url = new URL('https://freelance.habr.com/tasks.json')
       if (category) url.searchParams.set('categories', category)
       if (query) url.searchParams.set('q', query)
-      const response = await fetch(url);
-      const data = await response.json();
+      const response = await fetch(url)
+      const data = await response.json()
       data.forEach(task => {
         if (!lastTasks[task.id]) {
-          tasksToNotify.push(task);
-          lastTasks[task.id] = task;
+          tasksToNotify.push(task)
+          lastTasks[task.id] = task
         };
-      });
+      })
     }
     tasksToNotify.forEach(task => {
       const message = fmt`
@@ -96,23 +96,23 @@ ${bold`Price:`} ${task.price}
 ${bold`Published At:`} ${new Date(task.published_at).toLocaleString()}
 ${bold`Comments Count:`} ${task.task_comments_count}
 ${bold`Page Views:`} ${task.page_views_count}
-${bold`Link:`} ${link("View Task", task.url)}
-    `;
-      bot.telegram.sendMessage(userId, message);
-      console.log('New task added:', message.text);
-    });
+${bold`Link:`} ${link('View Task', task.url)}
+    `
+      bot.telegram.sendMessage(userId, message)
+      console.log('New task added:', message.text)
+    })
   }
   if (tasksToNotify.length === 0) console.log('No new tasks added')
-  else console.log('New tasks added:', tasksToNotify.length);
+  else console.log('New tasks added:', tasksToNotify.length)
 }
 
 async function main() {
-  await bot.telegram.setMyCommands(commands);
-  bot.launch();
-  console.log('Bot started!');
-  setInterval(checkForNewTasks, 60000);
-  process.once('SIGINT', () => bot.stop('SIGINT'));
-  process.once('SIGTERM', () => bot.stop('SIGTERM'));
+  await bot.telegram.setMyCommands(commands)
+  bot.launch()
+  console.log('Bot started!')
+  setInterval(checkForNewTasks, 60000)
+  process.once('SIGINT', () => bot.stop('SIGINT'))
+  process.once('SIGTERM', () => bot.stop('SIGTERM'))
 }
 
 main()
